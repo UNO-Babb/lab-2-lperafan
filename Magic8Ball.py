99f0e8e4ce4b42f29ec376c390b9982b8b9d1a8a
lperafan <lperafan@unomaha.edu>
2025-01-29 15:44:47 +0000

    Class Practice

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -10,9 +10,19 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
+  answers = ["As I see it, yes.", "Ask again later.", "Better not tell you know." "Cannnot predict now.", "Concentrate and ask again.", 
+  "Don't count on it.", "It is certain.", "It is decidedly so."]
+  # Each item must be in quotes, serparated by a comma.
   #Answer question randomly with one of the options from your earlier list.
 
+  num = random.random() #decimal 0 - 1
+  num = num * 100 #number 0 - 999 with decimal
+  num = int(num) # no more decimals
+
+  things = len(answers)
+  num = num % things
+  question = input("Ask me a question: ")
+  print(answers[num])
 
 if __name__ == '__main__':
   main()
